8e184000159f64b6322e7fc98a13a10bb08e30fd
Chetan Raghav <103754343+che-sinebycos@users.noreply.github.com>
2024-08-14 00:36:52 +0530

    Create find-peak-element.java

---
+++ Sorting and Searching/find-peak-element.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int n = nums.length;
+
+        if(n == 1) {
+            return 0;
+        }
+
+        if(nums[0] > nums[1]) {
+            return 0;
+        }
+        if(nums[n-1] > nums[n-2]) {
+            return n-1;
+        }
+
+        int lo = 0, hi = n-1;
+        while(lo <= hi) {
+            int mid = lo + (hi - lo)/2;
+
+            if(nums[mid] > nums[mid+1] && nums[mid] > nums[mid-1]) {
+                return mid;
+            }
+            else if(nums[mid+1] > nums[mid]) {
+                lo = mid+1;
+            }
+            else {
+                hi = mid-1;
+            }
+        }
+        return -1;
+    }
+}
