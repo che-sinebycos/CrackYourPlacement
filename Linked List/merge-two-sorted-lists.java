5e078ee709b004cfaf82bb71c4fc870a42acb176
Chetan Raghav <103754343+che-sinebycos@users.noreply.github.com>
2024-09-10 23:43:30 +0530

    Create merge-two-sorted-lists.java

---
+++ Linked List/merge-two-sorted-lists.java
@@ -0,0 +1,32 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null) {
+            return list2;
+        }
+        if(list2 == null) {
+            return list1;
+        }
+
+        ListNode dummy = new ListNode(-101);
+        ListNode temp = dummy;
+        while(list1 != null && list2 != null) {
+            if(list1.val <= list2.val) {
+                temp.next = list1;
+                temp = temp.next;
+                list1 = list1.next;
+            }
+            else {
+                temp.next = list2;
+                temp = temp.next;
+                list2 = list2.next;
+            }
+        }
+        if(list1 == null) {
+            temp.next = list2;
+        }
+        if(list2 == null) {
+            temp.next = list1;
+        }
+        return dummy.next;
+    }
+}
